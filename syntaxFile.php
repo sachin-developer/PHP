2218013dd86652769ce7162e04a44153a0867c5d
Sachin Singh <sachin.singh@superzop.com>
2021-10-06 11:07:14 +0530

    Syntax about echo and print

---
+++ syntaxFile.php
@@ -0,0 +1,18 @@
+<?php 
+echo "Sachin Singh <br>"; 
+// Echo is to display message or information as a output on display / screen
+
+echo "My name is ", "Sachin", " ", "Singh <br>" ;
+/*Echo can able to combine and print multiple messages
+seperated by comma(,)
+*/ 
+
+print "Sachin Singh <br>";
+// print also to display message or information as a output on display / screen
+
+
+//print "My name is ", "Sachin", " ", "Singh <br>" ; uncomment and check code here
+/*print is not able to combine and print multiple messages
+seperated by comma(,)
+*/ 
+?>
